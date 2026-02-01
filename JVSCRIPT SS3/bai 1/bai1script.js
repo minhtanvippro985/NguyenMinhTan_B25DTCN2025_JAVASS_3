507186beccf42e0b2ae37ce28af39e8f6dc6448c
minhtanvippro985 <minhtannguyen173@gmail.com>
2026-02-01 18:14:17 +0700

    Update bai1script.js

--- JVSCRIPT SS3/bai 1/bai1script.js
+++ JVSCRIPT SS3/bai 1/bai1script.js
@@ -8,15 +8,16 @@ for (let i = 0  ; i <= borrowCount ; i++ ){
     let bookName = prompt(`Lượt ${i + 1} - Tên sách:`);
 let days ;
 do {
-    days = +prompt("Nhap so ngay muon (1-30)");
+    days = +prompt("Nhập số ngày mượn (1-30)");
 
 } while (isNaN(days) || days < 1 || days >30 );
 
 if(days > 14){
-    console.log(`${userName}  , co sach${bookName} co thoi gian muon vuot muc `);
-    alert(`${userName}  , co sach${bookName} KHONG MUON DUOC SACH `)
+    console.log(`${userName}  , có sách${bookName} có thời gian vượt mức`);
+    alert(`${userName}  , có sách${bookName} KHÔNG DUỢC MƯỢN SÁCH `)
 } else {
-     console.log(`${userName}  , co sach${bookName} DUOC PHEP muon sach `);
-    alert(`${userName}  , co sach${bookName} da muon THANH CONG!`)
+     console.log(`${userName}  , CÓ SÁCH ${bookName} ĐƯỢC PHÉP MƯỢN SÁCH `);
+    alert(`${userName}  , có sách ${bookName} ĐÃ MƯỢN THÀNH CÔNG!`)
 }
+
 }
