c52459a423a4a913d9cf539b5d11ae167ff277d7
minhtanvippro985 <minhtannguyen173@gmail.com>
2026-02-01 18:11:14 +0700

    Update bai3script.js

--- JVSCRIPT SS3/bai 3/bai3script.js
+++ JVSCRIPT SS3/bai 3/bai3script.js
@@ -8,36 +8,36 @@ let successCount = 0 ;
 
 do {
 
-    addRequest = parseInt(prompt("1. Them moi \n2. Dung"));
+    addRequest = parseInt(prompt("1. Thêm moi \n2. Dừng"));
 
     if (isNaN(addRequest) || addRequest < 1 || addRequest > 2) {
-        console.log("Vui long chi nhap so tu 1-2");
+        console.log("Chi nhập số từ 1-2");
         continue;
     }
 
     if (addRequest === 1) {
-        userName = prompt("Nhap ten ban doc");
-        bookName = prompt("Nhap ten sach");
+        userName = prompt("Nhập tên bạn đọc");
+        bookName = prompt("Nhập tên bạn đọc");
         do {
-            baseDate = parseInt(prompt("Nhap ngay muon >= 1"));
+            baseDate = parseInt(prompt("Nhập số ngày muợn >= 1"));
             if (isNaN(baseDate) || baseDate <= 0) {
-                console.log("Ngay khong hop le");
+                console.log("Ngay không hợp lệ");
             }
         } while (isNaN(baseDate) || baseDate <= 0);
         
         do {
-            additionalDate = parseInt(prompt("Nhap ngay muon >= 1"));
+            additionalDate = parseInt(prompt("Nhâp ngày thêm >= 1"));
             if (isNaN(additionalDate) || additionalDate <= 0) {
-                console.log("Ngay them khong hop le");
+                console.log("Ngày thêm không hợp lệ");
             }
         } while (isNaN(additionalDate) || additionalDate <= 0);
         
 
         if(baseDate + additionalDate > 60){
-            console.log(`${userName} khong duoc gia han (vuot toi da)`);
+            console.log(`${userName} không được gia han (vượt tối đa)`);
             failCount++ ;
         } else if( baseDate + additionalDate > 45  ){
-             console.log(`${userName} khong duoc gia han (qua lau)`);
+             console.log(`${userName} không được gia hạn (quá lâu)`);
              failCount++;
 
         } else {
@@ -49,4 +49,5 @@ do {
 } while (addRequest !== 2); 
 console.log("--- THỐNG KÊ CA LÀM VIỆC ---");
 console.log(`Số lần gia hạn thành công: ${successCount}`);
+
 console.log(`Số lần gia hạn không thành công: ${failCount}`);
